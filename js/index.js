7990edfc487692131e166ff5fb7d835f54f44489
nicolaspenagos <nicolas.penagosm98@gmail.com>
2021-06-01 01:22:17 -0500

    Marker feature has been added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const mymap = L.map('mapid').setView([3.43722, -76.5225], 14);
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(tileUrl, { attribution });
+const msgInput = document.querySelector('.msg');
 
 tiles.addTo(mymap);
--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,10 @@
 const modal = document.querySelector('.modal');
 const modalContent = document.querySelector('.modal__content');
 const closeModal = document.querySelector('.closeModal');
+let lat = 0.0;
+let lon = 0.0;
+
+
 
 closeModal.addEventListener('click', handleCloseModal);
 
@@ -18,6 +22,7 @@ function handleCloseModal() {
     modalContent.style.transform = 'translate(0px, -500px)';
     document.body.style.overflow = 'hidden scroll';
     document.body.style.overflowX = 'hidden';
+    putMarker();
     setTimeout(function() {
         modal.style.display = 'none';
     }, 500);
@@ -28,6 +33,20 @@ function handleModalAppear() {
 
     modal.style.opacity = 1;
     modalContent.style.transform = 'translate(0px, 0px)';
+
+}
+
+function putMarker() {
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        mymap.setView([lat, lon], 18);
+        var marker = L.marker([lat, lon]).addTo(mymap);
+        var msg = msgInput.value;
+        var popup = marker.bindPopup(`<b>Hello world!</b><br/>${msg}`);
+        popup.openPopup();
+    });
 
 }
 
