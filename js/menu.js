451cc46ca403590087620a0dcbca398c604da1be
nicolaspenagos <nicolas.penagosm98@gmail.com>
2021-06-11 00:21:50 -0500

    Map update

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,11 @@
 const goBackBtn = document.querySelector('.goBack');
+const showMapBtn = document.querySelector('.seeMap');
+
 
 goBackBtn.addEventListener('click', () => {
     window.location.href = './index.html';
+});
+
+showMapBtn.addEventListener('click', () => {
+    window.location.href = './map.html';
 });
